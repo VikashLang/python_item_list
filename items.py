fb3305c110dbdc9e48c42986f6659e527193779b
VikashLang <vikashlang03@gmail.com>
2024-12-10 00:07:47 +0530

    Add files via upload

---
+++ items.py
@@ -0,0 +1,136 @@
+import os
+os.makedirs("items", exist_ok=True)
+items = [
+    {
+        "name": "Arthur's sword",
+        "cost": 145.99,
+        "main_image_name": "arthur's sword.jpg",
+        "hover_image_name": "arthur's sword hover.jpg"
+    },
+    {
+        "name": "Merlin's staff",
+        "cost": 200.50,
+        "main_image_name": "Merlin's staff.jpg",
+        "hover_image_name": "Merlin_s_staff_hover-removebg-preview.png"
+    },
+    {
+        "name": "Dragon shield",
+        "cost": 300.75,
+        "main_image_name": "Dragon shield.jpg",
+        "hover_image_name": "Dragon shield hover.jpg"
+    },
+    {
+        "name": "Elven bow",
+        "cost": 120.00,
+        "main_image_name": "Elven bow.jpg",
+        "hover_image_name": "Elven_bow_hover-removebg-preview.png"
+    },
+    {
+    "name": "Aetherforge Gauntlets",
+        "cost": 250.99,
+        "main_image_name": "Aetherforge Gauntlets.jpg",
+        "hover_image_name": "Aetherforge_Gauntlets_hover-removebg-preview.png"
+    },
+    {
+    "name": "Glacial Prism Amulet",
+        "cost": 500.75,
+        "main_image_name": "Glacial Prism Amulet.jpg",
+        "hover_image_name": "Glacial_Prism_Amulet_hover-removebg-preview.png"
+    },
+     {
+    "name": "Sharbreaker Warhammer",
+        "cost": 424.89,
+        "main_image_name": "Sharbreaker Warhammer.jpg",
+        "hover_image_name": "Sharbreaker_Warhammer_hover-removebg-preview.png"
+    },
+     {
+    "name": "Moonveil Elixir",
+        "cost": 349.99,
+        "main_image_name": "Moonveil Elixir.jpg",
+        "hover_image_name": "Moonveil_Elixir_hover-removebg-preview.png"
+    },
+    {
+    "name": "Ashenflame Torch",
+        "cost": 431.45,
+        "main_image_name": "aashenflame torch.jpeg",
+        "hover_image_name": "aashfame_torch-removebg-preview.png"
+    },
+    {
+    "name": "Blazing Fang Dagger",
+        "cost": 121.11,
+        "main_image_name": "blazing fang dagger.jpeg",
+        "hover_image_name": "blazing_fang_dagger_hover-removebg-preview.png"
+    },
+    {
+    "name": "Crystal Shard Of Time",
+        "cost": 664.32,
+        "main_image_name": "crystal shard of time.jpeg",
+        "hover_image_name": "crystal_shard_of_time_hover-removebg-preview.png"
+    },
+    {
+    "name": "Elixir Of Enternal Night",
+        "cost": 444.21,
+        "main_image_name": "elixir of enternal night.jpeg",
+        "hover_image_name": "elixir_of_enternal_night_hover-removebg-preview.png"
+    },
+    {
+    "name": "Moonlight Circlet",
+        "cost": 234.56,
+        "main_image_name": "moonlight circlet.jpeg",
+        "hover_image_name": "moonligh_criclet_-removebg-preview.png"
+    },
+    {
+    "name": "Obsidian Rune Ring",
+        "cost": 234.65,
+        "main_image_name": "obsidian rune ring.jpeg",
+        "hover_image_name": "obsidian_rune_ring_hover-removebg-preview.png"
+    },
+    {
+    "name": "Phoenixfeather Arrow",
+        "cost": 112.42,
+        "main_image_name": "phoenixfeather arrow.jpeg",
+        "hover_image_name": "phoenixfeather_arrow_hover-removebg-preview.png"
+    },
+    {
+    "name": "Shadow Binder's Cloak",
+        "cost": 678.31,
+        "main_image_name": "shadow binder's claok.jpeg",
+        "hover_image_name": "kingpin-removebg-preview.png"
+    },
+    {
+    "name": "Soulkeeper's Locket",
+        "cost": 767.43,
+        "main_image_name": "soulkeeper's locket.jpeg",
+        "hover_image_name": "soulskeeper_locket_hover-removebg-preview.png"
+    },
+    {
+    "name": "Starlight Lantern",
+        "cost": 112.32,
+        "main_image_name": "starlight lantern'.jpeg",
+        "hover_image_name": "starlight_lantern_hover-removebg-preview.png"
+    },
+    {
+    "name": "Thornweave Bracers",
+        "cost": 645.34,
+        "main_image_name": "hornweave bracers.jpeg",
+        "hover_image_name": "thornweave_bracers_hover-removebg-preview.png"
+    },
+    {
+    "name": "Thunderclap Hammer",
+        "cost": 994.21,
+        "main_image_name": "thunderclap hammer.jpeg",
+        "hover_image_name": "thunderclap_hammer_hover-removebg-preview.png"
+    },
+    {
+    "name": "Thunderforge Axe",
+        "cost": 122.43,
+        "main_image_name": "thunderforge axe.jpeg",
+        "hover_image_name": "thunderforge_axe_hover-removebg-preview.png"
+    },
+    {
+    "name": "Venomblood Potion",
+        "cost": 312.23,
+        "main_image_name": "venomblood potion.jpeg",
+        "hover_image_name": "venomblood_potion_hover-removebg-preview.png"
+    },
+]
